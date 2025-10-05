8e68df000cfae7c3dd417735689ea5122c8451df
ajay1951 <48474193+ajay1951@users.noreply.github.com>
2025-10-05 14:52:24 +0530

    fix: Correct axios config for deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,26 +1,15 @@
-
 import axios from 'axios';
 
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + '/api', // This will be prefixed to all request URLs
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: process.env.REACT_APP_API_URL, // e.g., https://your-backend.onrender.com
 });
 
-
-api.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['x-auth-token'] = token;
-    }
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['x-auth-token'] = token;
   }
-);
+  return config;
+});
 
 export default api;
